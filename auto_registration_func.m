046d5bb4e8dd1430ea6300da54aa9e3f19186edc
SamuelHLam <sam1980lam@gmail.com>
2019-08-01 11:53:44 -0400

    Updated matlab script to crop

--- auto_registration_func.m
+++ auto_registration_func.m
@@ -1,19 +1,20 @@
 %fn1 = 'qupath.png';
 %fn2 = 'asap.png';
 
-function auto_registration_func (fn1,fn2)
+function auto_registration_func (f1,f2, name)
+
+fn1 = sprintf('%s.png',f1)
+fn2 = sprintf('%s.png',f2)
 
 fn1_out = ['r_' fn1];
 fn2_out = ['r_' fn2];
 
-fn_result = sprintf('%s_%s',fn1,fn2);
-
 fn_reg = 'mtreg.mat';
 
-qupath = imread(fn1);
-ndp = imread(fn2);
+mov = imread(fn1);
+fix = imread(fn2);
 
-movingReg = registerImages(qupath, ndp);
+movingReg = registerImages(mov, fix);
 
 movingReg.Transformation.T
 save(fn_reg,'movingReg')
@@ -25,13 +26,13 @@ imwrite(r_mov, fn1_out)
 imwrite(r_fix, fn2_out)
 dE = compareColorFunction_LAB(fn1_out, fn2_out);
 
-save(sprintf('dE_%s.mat',fn_result),'dE')
+save(sprintf('dE_%s.mat',name),'dE')
 
 caxis([0 30])
 set(gca,'visible','off')
 set(gca,'xtick',[])
 set(gcf,'Position',[0 0 flip(size(dE))])
-saveas(gcf, sprintf('%s%s',fn1,fn2))
+saveas(gcf, sprintf('%s.png',name))
 
 return
 
--- auto_registration_func_do6.m
+++ auto_registration_func_do6.m
@@ -1,11 +1,19 @@
-fn0 = 'ndpview2.png';
-fn1 = 'asap.png';
-fn2 = 'qupath.png';
-fn3 = 'sedeen.png';
+f0 = 'qupath';
+f1 = 'asap';
+f2 = 'sedeen';
+f3 = 'ndpview2';
 
-auto_registration_func(fn1, fn0)
-auto_registration_func(fn2, fn0)
-auto_registration_func(fn3, fn0)
-auto_registration_func(fn1, fn2)
-auto_registration_func(fn2, fn3)
-auto_registration_func(fn1, fn3)
+ndp = imread('ndpview2.png');
+ndp_crop = imcrop(ndp, [750, 330, 400, 400]);
+imwrite(ndp_crop, 'ndpview2.png');
+
+auto_registration_func(f0, f3, 'QuPath-NDP')
+auto_registration_func(f1, f3, 'ASAP-NDP')
+auto_registration_func(f2, f3, 'Sedeen-NDP')
+
+f2 = 'r_sedeen';
+auto_registration_func(f0, f2, 'QuPath-Sedeen')
+auto_registration_func(f1, f2, 'ASAP-Sedeen')
+
+f1 = 'r_asap';
+auto_registration_func(f0, f1, 'QuPath-ASAP')
