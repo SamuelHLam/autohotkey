5969884f4bf4645a42a627fc3c35f310ad61c9c3
SamuelHLam <33165333+SamuelHLam@users.noreply.github.com>
2019-08-01 11:26:30 -0400

    Add files via upload

---
+++ auto_registration_func.m
@@ -0,0 +1,44 @@
+%fn1 = 'qupath.png';
+%fn2 = 'asap.png';
+
+function auto_registration_func (fn1,fn2)
+
+fn1_out = ['r_' fn1];
+fn2_out = ['r_' fn2];
+
+fn_result = sprintf('%s_%s',fn1,fn2);
+
+fn_reg = 'mtreg.mat';
+
+qupath = imread(fn1);
+ndp = imread(fn2);
+
+movingReg = registerImages(qupath, ndp);
+
+movingReg.Transformation.T
+save(fn_reg,'movingReg')
+
+[r_mov, r_fix] = ImageRegistration(fn1,fn2,fn_reg);
+quality = registrationQualityEvaluationVariable(r_mov, r_fix)
+
+imwrite(r_mov, fn1_out)
+imwrite(r_fix, fn2_out)
+dE = compareColorFunction_LAB(fn1_out, fn2_out);
+
+save(sprintf('dE_%s.mat',fn_result),'dE')
+
+caxis([0 30])
+set(gca,'visible','off')
+set(gca,'xtick',[])
+set(gcf,'Position',[0 0 flip(size(dE))])
+saveas(gcf, sprintf('%s%s',fn1,fn2))
+
+return
+
+% load('dE','diffMatrix')
+
+% surf(diffMatrix)
+
+return
+
+end
---
+++ auto_registration_func_do6.m
@@ -0,0 +1,11 @@
+fn0 = 'ndpview2.png';
+fn1 = 'asap.png';
+fn2 = 'qupath.png';
+fn3 = 'sedeen.png';
+
+auto_registration_func(fn1, fn0)
+auto_registration_func(fn2, fn0)
+auto_registration_func(fn3, fn0)
+auto_registration_func(fn1, fn2)
+auto_registration_func(fn2, fn3)
+auto_registration_func(fn1, fn3)
