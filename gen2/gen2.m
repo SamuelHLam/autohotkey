1568fcc586aaf97eccd678de5ba2c46ba1d41a14
SamuelHLam <sam1980lam@gmail.com>
2019-08-22 11:59:29 -0400

    implemented sedeen

--- gen2/gen2.m
+++ gen2/gen2.m
@@ -1,23 +1,75 @@
-function gen2
-
+global root
 root = cd
 delete(strcat(root, "\qupath.png"))
+delete(strcat(root, "\sedeen.png"))
 
 % use ActiveX
+global server
 server = actxserver('WScript.Shell');
 
 server.Run("autohotkey.exe viewer_interface.ahk");
 
-qupath(server);
-end
+sedeen;
+%qupath;
+server.SendKeys('{ESC}');
 
-
-function qupath(server)
-root = cd;
-cd('C:\Program Files\QuPath');
-20
+function sedeen
+global root
+global server
 
 % open QuPath
+cd('C:\Program Files\Sedeen Viewer');
+server.Run('sedeen.exe');
+pause(5)
+
+% focus on the window
+server.AppActivate('Sedeen Viewer');
+
+% return to root directory
+cd(root);
+
+% open file
+server.SendKeys('^o');
+pause(1)
+
+server.SendKeys(strcat(root, '\CMU-1.ndpi'));
+pause(1)
+
+server.SendKeys('%+o');
+pause(5)
+
+% removes tabs and opens zoom bar
+server.SendKeys('%vl{DOWN}~%vt{DOWN}~');
+pause(1)
+
+% call AHK to click button to zoom to 20x
+command = 'click.exe 30 250';
+system(command);
+pause(1)
+
+% closes zoom bar and opens slide map
+server.SendKeys('%vt{DOWN}~%vt~');
+pause(1)
+
+% call AHK to move FOV
+server.SendKeys('%+s');
+pause(27)
+
+% closes slide map
+server.SendKeys('%vt~');
+
+snip(server, strcat(root, "\sedeen.png"));
+
+command = 'exit.exe sedeen.exe';
+system(command);
+end
+
+function qupath
+global root
+global server
+
+% open QuPath
+cd('C:\Program Files\QuPath');
 server.Run('QuPath.exe');
 pause(5)
 
@@ -34,7 +86,7 @@ cd(root);
 server.SendKeys('^o');
 pause(1)
 
-server.SendKeys('C:\Users\wcc\Documents\GitHub\autohotkey\gen2\CMU-1.ndpi');
+server.SendKeys(strcat(root, '\CMU-1.ndpi'));
 pause(1)
 
 server.SendKeys('%+o');
@@ -66,18 +118,20 @@ server.SendKeys('{ESC}');
 end
 
 function snip(server, save_path)
-% start SnippingTool
+% start snipping tool
 server.Run('SnippingTool');
 
 % focus on the window
 server.AppActivate('Snipping Tool');
 pause(1)
 
+% full screen snip
 server.SendKeys('%n{UP}~^s');
 pause(2)
 server.SendKeys(save_path);
-pause(1)
 server.SendKeys('~');
 pause(1)
+
+% exit snipping tool
 server.SendKeys('%fx');
 end
