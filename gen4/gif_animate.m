764d0f1cb7cc9b81576972cc417f5ff06f1433a6
waynewccheng at NUC2 <45103074+waynewccheng@users.noreply.github.com>
2020-08-25 09:36:55 -0400

    gif_animate
    handle grayscale images with optional output filename

--- gen4/gif_animate.m
+++ gen4/gif_animate.m
@@ -1,10 +1,20 @@
 % GIF flipping between two images
-function gif_animate (fn1, fn2)
+% 8-25-2020: handle grayscale images with optional output filename
 
-fn3 = 'test.gif'
+function gif_animate (fn1, fn2, fnout)
+
+if nargin < 2
+    disp('need two image filenames')
+    return
+end
+
+if nargin < 3
+    fnout = 'test.gif';
+end
 
 % Capture the plot as an image
 im1 = imread(fn1);
+im1 = gray2color(im1);
 image(im1);
 axis image
 axis off
@@ -14,10 +24,11 @@ im = frame2im(frame);
 [imind,cm] = rgb2ind(im,256);
 
 % Write frames to the GIF File
-imwrite(imind,cm,fn3,'gif','Loopcount',inf);
+imwrite(imind,cm,fnout,'gif','Loopcount',inf);
 
 % Capture the plot as an image
 im2 = imread(fn2);
+im2 = gray2color(im2);
 image(im2);
 axis image
 axis off
@@ -26,5 +37,15 @@ frame = getframe(gcf);
 im = frame2im(frame);
 [imind,cm] = rgb2ind(im,256);
 
-imwrite(imind,cm,fn3,'gif','WriteMode','append');
+imwrite(imind,cm,fnout,'gif','WriteMode','append');
+
+%
+% convert a gray image to color
+%
+    function imout = gray2color (imin)
+        if length(size(imin))==2
+            imout = repmat(double(imin)/255,1,1,3);
+        end
+        return
+    end
 end
